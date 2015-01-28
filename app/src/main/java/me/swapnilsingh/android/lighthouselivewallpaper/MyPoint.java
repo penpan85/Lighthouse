fe1d523870fec998708838fbe9bfd8534d94104b
Swapnil Singh <thisisnotaid@gmail.com>
2015-01-28 23:45:56 +0530

    Adds a basic android wallpaper

---
+++ app/src/main/java/me/swapnilsingh/android/lighthouselivewallpaper/MyPoint.java
@@ -0,0 +1,13 @@
+package me.swapnilsingh.android.lighthouselivewallpaper;
+
+public class MyPoint {
+    String text;
+    public float x;
+    public float y;
+
+    public MyPoint(String text, float x, float y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+}
