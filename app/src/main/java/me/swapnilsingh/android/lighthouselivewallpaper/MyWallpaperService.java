f17b27a9eefb68c97a7e7e9c1d6794a2a889a1db
Swapnil Singh <thisisnotaid@gmail.com>
2015-01-29 23:46:02 +0530

    Removes scaffold code

--- app/src/main/java/me/swapnilsingh/android/lighthouselivewallpaper/MyWallpaperService.java
+++ app/src/main/java/me/swapnilsingh/android/lighthouselivewallpaper/MyWallpaperService.java
@@ -1,12 +1,8 @@
 package me.swapnilsingh.android.lighthouselivewallpaper;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
@@ -29,26 +25,13 @@ public class MyWallpaperService extends WallpaperService {
             }
 
         };
-        private List<MyPoint> circles;
-        private Paint paint = new Paint();
         private int width;
         int height;
         private boolean visible = true;
-        private int maxNumber;
-        private boolean touchEnabled;
 
         public MyWallpaperEngine() {
             SharedPreferences prefs = PreferenceManager
                     .getDefaultSharedPreferences(MyWallpaperService.this);
-            maxNumber = Integer
-                    .valueOf(prefs.getString("numberOfCircles", "4"));
-            touchEnabled = prefs.getBoolean("touch", false);
-            circles = new ArrayList<MyPoint>();
-            paint.setAntiAlias(true);
-            paint.setColor(Color.WHITE);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeJoin(Paint.Join.ROUND);
-            paint.setStrokeWidth(10f);
             handler.post(drawRunner);
         }
 
@@ -79,27 +62,7 @@ public class MyWallpaperService extends WallpaperService {
 
         @Override
         public void onTouchEvent(MotionEvent event) {
-            if (touchEnabled) {
-
-                float x = event.getX();
-                float y = event.getY();
-                SurfaceHolder holder = getSurfaceHolder();
-                Canvas canvas = null;
-                try {
-                    canvas = holder.lockCanvas();
-                    if (canvas != null) {
-                        canvas.drawColor(Color.BLACK);
-                        circles.clear();
-                        circles.add(new MyPoint(String.valueOf(circles.size() + 1), x, y));
-                        drawCircles(canvas, circles);
-
-                    }
-                } finally {
-                    if (canvas != null)
-                        holder.unlockCanvasAndPost(canvas);
-                }
-                super.onTouchEvent(event);
-            }
+            super.onTouchEvent(event);
         }
 
         private void draw() {
@@ -108,14 +71,7 @@ public class MyWallpaperService extends WallpaperService {
             try {
                 canvas = holder.lockCanvas();
                 if (canvas != null) {
-                    if (circles.size() >= maxNumber) {
-                        circles.clear();
-                    }
-                    int x = (int) (width * Math.random());
-                    int y = (int) (height * Math.random());
-                    circles.add(new MyPoint(String.valueOf(circles.size() + 1),
-                            x, y));
-                    drawCircles(canvas, circles);
+
                 }
             } finally {
                 if (canvas != null)
@@ -124,14 +80,6 @@ public class MyWallpaperService extends WallpaperService {
             handler.removeCallbacks(drawRunner);
             if (visible) {
                 handler.postDelayed(drawRunner, 5000);
-            }
-        }
-
-        // Surface view requires that all elements are drawn completely
-        private void drawCircles(Canvas canvas, List<MyPoint> circles) {
-            canvas.drawColor(Color.BLACK);
-            for (MyPoint point : circles) {
-                canvas.drawCircle(point.x, point.y, 20.0f, paint);
             }
         }
     }
